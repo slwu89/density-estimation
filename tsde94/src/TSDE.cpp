8719aa3c9d7e76f8e3019ec08cf3abf42e635eb6
slwu89 <slwu89@berkeley.edu>
2019-04-12 08:53:30 -0700

    cleaning up code

--- tsde94/src/TSDE.cpp
+++ tsde94/src/TSDE.cpp
@@ -1,5 +1,5 @@
 /* ################################################################################
-#   TSDE
+#   TSDE algorithm
 ################################################################################ */
 
 /* Rcpp */
@@ -9,12 +9,25 @@
 #include <math.h>
 #include <iostream>
 
+
+/* ################################################################################
+#   Helper functions for dynamically allocated arrays
+################################################################################ */
+
 /* helper functions to allocate and free memory for arrays */
 double* make_array(const size_t len){
   return new double[len];
 }
 
+int* make_int_array(const size_t len){
+  return new int[len];
+}
+
 void free_array(double* array){
+  delete[] array;
+}
+
+void free_int_array(int* array){
   delete[] array;
 }
 
@@ -33,7 +46,11 @@ void free_2darray(double** array, const size_t nrow){
     delete[] array;
 }
 
-/* global and static variables */
+
+/* ################################################################################
+#   global parameters that are not adjusted very often
+################################################################################ */
+
 static const size_t MAXTREE = 30000;      /* The largest tree size */
 static const size_t MAXSAVE = 1000;       /* The largest tree to be saved */
 static const size_t MXLEVEL = 35;         /* The maximum of tree depth */
@@ -49,11 +66,14 @@ static double allarea = 0.0;  /* The total area */
 static double deltax = 0.0; /* The step length of each split */
 
 /* these are arrays */
-int nodleft[MAXTREE*MAXSAVE/8];
-int nodrigt[MAXTREE*MAXSAVE/8];
-double error33[MAXTREE*MAXSAVE/8];
+static int nodleft[MAXTREE*MAXSAVE/8];
+static int nodrigt[MAXTREE*MAXSAVE/8];
+static double error33[MAXTREE*MAXSAVE/8];
 
-/* nodes of tree */
+
+/* ################################################################################
+#   Nodes and the tree for density estimation
+################################################################################ */
 
 int   nodelst;        /* The node pointer */
 
@@ -85,14 +105,27 @@ struct node {
 	int    rowname;
 };
 
-struct node nodes[MAXTREE + 1];
+static struct node nodes[MAXTREE + 1];
 #define TWORK nodes[lnodes]
 
 
+/* ################################################################################
+#   function declarations (see definitions below main TSDE function)
+################################################################################ */
+
+void casplt(int lnodes);
+void msleft(int level, int ivar, int cutp, int lower, int upper);
+void msrigt(int level, int ivar, int cutp, int upper);
+void cutree();
+void smooth();
+void gttree();
+void trsort();
+void cutree();
 
 
-
-
+/* ################################################################################
+#   TSDE function (to be exported to R)
+################################################################################ */
 
 //' TSDE algorithm
 //'
@@ -137,9 +170,6 @@ void TSDE(
   size_t varstan_l = varmean_l;
   double* varstan = make_array(varstan_l); /* Standard deviations of the variables */
 
-  // double dmatrix[nmlearn + 1][nmbvars + 1];  /* The working data set */
-  // double varmean[nmbvars + 1];   /* Means of the variables */
-  // double varstan[nmbvars + 1];   /* Standard deviations of the variables */
 
   /* standardize the input data */
   for(ivar=1;ivar<=nmbvars;ivar++) {
@@ -179,13 +209,58 @@ void TSDE(
   }
   // END DEBUGGING
 
+  /* define more dynamically allocated arrays */
+  size_t ccsplit_l = nmbcuts + 2;
+  double* ccsplit = make_array(ccsplit_l);
 
+  size_t ndlower_l = nmbvars + 1;
+  int* ndlower = make_int_array(ndlower_l);  /* The moving lower bound of the node */
+
+  size_t ndupper_l = nmbvars + 1;
+  int* ndupper = make_int_array(ndupper_l);  /* The moving upper bound of the node */
+
+  size_t cutmass_l = nmbcuts + 1;
+  double* cutmass = make_array(cutmass_l);  /* The moving mass in split */
+
+  size_t density_r = MXLEVEL + 2;
+  size_t density_c = nmlearn + 1;
+  double** density = make_2darray(density_r, density_c);  /* The moving density values of the node */
+  size_t smatrix_r = nmlearn + 1;
+  size_t smatrix_c = nmbvars + 1;
+  double** smatrix = make_2darray(smatrix_r, smatrix_c);
+  size_t lmatrix_r = nmlearn + 1;
+  size_t lmatrix_c = nmbvars + 1;
+  double** lmatrix = make_2darray(lmatrix_r, lmatrix_c);
+  size_t umatrix_r = nmlearn + 1;
+  size_t umatrix_c = nmbvars + 1;
+  double** umatrix = make_2darray(umatrix_r, umatrix_c);
+
+  size_t fmatrix_l = nmlearn * nmbvars * nmbcuts / 5;
+  double* fmatrix = make_array(fmatrix_l);
+
+  /* Prepare the split information */
+	deltax = (upper00 - lower00) / (nmbcuts + 1);   /* x is the step length */
+	for(k=0;k<=(nmbcuts+1);k++) ccsplit[k] = k * deltax + lower00;
+	x = upper00 - lower00;
+	allarea = pow(x, (double) nmbvars);
 
 
   /* clean up dynamically allocated memory */
   free_2darray(dmatrix,dmatrix_r);
   free_array(varmean);
   free_array(varstan);
+
+  free_array(ccsplit);
+  free_int_array(ndlower);
+  free_int_array(ndupper);
+  free_array(cutmass);
+
+  free_2darray(density, density_r);
+  free_2darray(smatrix, smatrix_r);
+  free_2darray(lmatrix, lmatrix_r);
+  free_2darray(umatrix, umatrix_r);
+
+  free_array(fmatrix);
 
   // for(size_t i=1; i<=nmlearn; i++){
   //   delete [] dmatrix[i];
